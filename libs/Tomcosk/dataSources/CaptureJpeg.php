66d371df3e0590de43ddcf2c23d7031e5ba1d637
Tomas Igrini <tomco@tomco.sk>
2015-12-12 17:12:26 +0100

    added option to capture image when url provide only temporary url of image.

--- libs/Tomcosk/dataSources/CaptureJpeg.php
+++ libs/Tomcosk/dataSources/CaptureJpeg.php
@@ -9,12 +9,14 @@ class CaptureJpeg extends DataSource
 
 	protected $url;	
 	protected $html;
+	protected $type = null;
 	protected $name = "CaptureJpeg";
 	protected $description = "Capture frame from JPEG URL";
 
-	function __construct($url)
+	function __construct($url, $type="image")
 	{
 		$this->setUrl($url);
+		$this->type = $type;
 		return $this;
 	}
 
@@ -38,12 +40,21 @@ class CaptureJpeg extends DataSource
 	public function apply($options) {
 		$folder = $options["folder"];
 		$filename = $options["filename"];
+
+		if($this->type == "url") {
+			$url = file($this->url);
+			$pattern = '/"([^"]+)"/';
+			$url = preg_match($pattern, $url[0], $matches);
+			$url = $matches[1];
+			$this->setUrl($url);
+		}
+
 		$opts=array(
 		    "ssl"=>array(
 		    "verify_peer"=>false,
 		    "verify_peer_name"=>false,
 		    ),
-		); 		
+		);
 		file_put_contents($folder."/".$filename, fopen($this->url, 'r', false, stream_context_create($opts)));
 		$this->log($this->url, 2);
 		$this->log("Frame captured");
